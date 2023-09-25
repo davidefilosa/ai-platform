84deebddc19afeee0a4cd92afd78e7db30305e49
Davide Filosa <davideclaypool@gmail.com!>
2023-09-25 14:41:05 +0200

    created dashboard page

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -10,8 +10,19 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <Sheet>
