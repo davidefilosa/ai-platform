f32934eade75910f7d65d7a1e3bc4f4ec1e55f63
Davide Filosa <davideclaypool@gmail.com!>
2023-09-26 08:15:59 +0200

    completed conversation page

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,11 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+const BotAvatar = () => {
+  return (
+    <Avatar className="w-6 h-12">
+      <AvatarImage src="/logo.png" />
+    </Avatar>
+  );
+};
+
+export default BotAvatar;
---
+++ components/loader.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const Loader = () => {
+  return (
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="w-4 h-16 relative animate-bounce">
+        <Image alt="Logo" src="/logo.png" width={16} height={32} />
+      </div>
+      <p className="text-sm text-muted-foreground">Genius is thinking...</p>
+    </div>
+  );
+};
