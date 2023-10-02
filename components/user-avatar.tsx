e8f6a63d8ccf2da3c4dbbd4145465544ccb8fe8e
Davide Filosa <davideclaypool@gmail.com!>
2023-10-02 09:38:25 +0200

    fixed bug user-avatar

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -8,8 +8,8 @@ const UserAvatar = () => {
     <Avatar className="w-8 h-8">
       <AvatarImage src={user?.profileImageUrl} />
       <AvatarFallback>
-        {user.firstName?.charAt(0)}
-        {user.lastNmae?.charAt(0)}
+        {user?.firstName?.charAt(0)}
+        {user?.lastName?.charAt(0)}
       </AvatarFallback>
     </Avatar>
   );
