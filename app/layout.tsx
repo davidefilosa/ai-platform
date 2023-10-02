2dcdd1d9455c9f25a16cd33b5841a0d7ebb55fae
Davide Filosa <davideclaypool@gmail.com!>
2023-10-02 09:30:25 +0200

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import ModalProvider from "@/components/modal-provider";
 import ToastProvider from "@/components/toast-provider";
 import CrispProvider from "@/components/crisp-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           <ToastProvider />
           <ModalProvider />
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
